c0103378a4dc8b9e14f3867aff0fb2ec6fd2d95a
K-Erlan <73570898+K-Erlan@users.noreply.github.com>
2020-12-13 22:23:27 +0300

    Connection db with python and some functions

--- main.py
+++ main.py
@@ -12,23 +12,85 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.spinner import Spinner
 from kivy.animation import Animation
 
-import pyodbc
-conn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=LENOVO\CHIKI;'
-                      'Database=forApp;'
-                      'Trusted_Connection=yes;')
-cursor = conn.cursor()
-cursor.execute('SELECT * FROM forApp.dbo.listOfThings')
-
-for row in cursor:
-    print(row)
+import MySQLdb
 
 class MainView(ScrollView):
+  desc_id = False
+  desc_name = False
+  desc_date = False
 
-	desc_id = False
-	desc_name = False
-	desc_date = False
+  q = '''
+    SELECT * FROM todo.list_of_things
+    ORDER BY list_of_things.id
+  '''
 
-	q = '''
-		SELECT 
-		'''
+  con = MySQLdb.connect(host='127.0.0.1', user='root', passwd='aruna2010', db='todo')
+  cur = con.cursor()
+  cur.execute('SET NAMES utf8')
+
+  def add_record(self, vals):
+    alert = 0
+    for val in vals:
+      if len(val) == 0:
+        alert = 1
+        break
+
+    if alert == 0:
+      q = self.query("INSERT INTO todo.list_of_things (id, checklist, date%time, status) VALUES ('%d', '%s', '%d', '%s')"
+        %(int(vals[0]), str(vals[1]), int(vals[2]), str(vals[3])))
+      self.fill(self.db_data(q))
+      self.qchange()
+    else:  
+      pass
+
+  def del_record(self, vals):
+    date_validation = False
+    i = 0
+    for val in vals:
+      if len(val) > 0:
+        break
+      i += 1
+    if i == 0:
+      q = self.query("DELETE FROM todo.list_of_things WHERE checklist = '%s'" %str(vals[i]))
+      self.fill(self.db_data(q))
+      self.qchange()
+    elif i == 1:
+      try:
+        parts = str(vals[i]).split("-")
+        string = ''
+        for item in parts:
+          string += item
+        if len(string) == 8:
+          date_validation = True
+        else:
+          pass  
+      except:
+        pass
+      if date_validation is True:
+        q = self.query("DELETE FROM todo.list_of_things WHERE founded = '%s'" %str(vals[i]))
+        self.fill(self.db_data(q))
+        self.qchange()
+      else:  
+        pass
+    else:
+      pass
+      
+  def qcahnge(self):
+    if self.desc_id is False:
+      q = self.query('''
+      SELECT * FROM todo.list_of_things
+      ORDER BY todo.list_of_tjings.id'''          
+              )
+      self.desc_id = True
+    else:
+      q = self.query('''
+        SELECT * FROM todo.list_of_things
+        ORDER BY id'''
+              )
+      self.desc_id = False
+
+    self.fill(self.db_data(q))
+
+  def qchange2(self):
+    if self.desc_name = False:
+      pass
