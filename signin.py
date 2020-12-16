ca3a61d56fe7f4a7b7f89756b818ab96426d6e63
Nuraim <62842779+Nuraim@users.noreply.github.com>
2020-12-16 14:58:22 +0600

    Updating signin buttons

--- signin.py
+++ signin.py
@@ -1,63 +1,37 @@
-<FlatButton@ButtonBehavior+Label>:
-	font_size: 16
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.image import Image
+from kivy.core.window import Window
 
-<SigninWindow>:
-	id: main_win
-	orientation: "vertical"
-	spacing: 10
-	space_x: self.size[0]/2.5
-	
-	BoxLayout:
-		size_hint_y: None
-		height: 50
-		canvas:
-			Color:
-				rgba: (.06, .45, .45, 1)
-			Rectangle:
-				size: self.size
-				pos: self.pos 	 
-		Label:
-			text: "Access Control"
-			bold: True 
-			size_hint_x: .9	
-		FlatButton:
-			text: "x"
-			size_hint_x: .1
-	BoxLayout:
-		orientation: 'vertical'
-		padding: main_win.space_x, 10 
-		# spacing: 20
-		BoxLayout:
-			orientation: "vertical"
-			spacing: 10
-			size_hint_y: None
-			height: 80
-			Label:
-				id: info
-				text: ''
-				markup: True
-			TextInput:
-				id: username_field
-				hint_text: "Username"
-				multiline: False 
-				focus: True
-				on_text_validate: pwd_field.focus = True
-			TextInput:
-				id: pwd_field
-				hint_text: "Password"
-				multiline: False
-				password: True
-				on_text_validate: root.validate_user()
-		Label:
-			id: sp
-			size_hint_y: None
-			height: 40
-		Button:
-			text: "Sign In"
-			size_hint_y: None
-			height: 40
-			background_color: (.06,.45,.45, 1)
-			background_normal: ''
-			on_release: root.validate_user()
-		Label:
-			id: sp2
+Window.size=(360,640)
+
+class SigninWindow(BoxLayout):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+
+	def validate_user(self):
+		user = self.ids.username_field
+		pwd = self.ids.pwd_field
+		info = self.ids.info
+
+		uname = user.text
+		passw = pwd.text
+
+		if uname == "" or passw == '':
+			info.text ='[color=#FF0000]username and/ or password required'
+		else:
+			if uname == 'admin' and passw == 'admin':
+				print ('Logged In successfully!!!')
+
+class FullImage(Image):
+	def build(self):
+		return Image(source='img.jpg')
+
+class SigninApp(App):
+	def build(self):
+		return SigninWindow()
+		return FullImage()
+
+if __name__=="__main__":
+	sa = SigninApp()
+	sa.run()
